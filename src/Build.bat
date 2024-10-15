34ba299087c3ac0a0e26460e0541682914334829
Fahad704 <shaikh.fahad.77377@gmail.com>
2024-10-15 14:04:46 +0530

    Added Optimization flags in build file

--- src/Build.bat
+++ src/Build.bat
@@ -2,7 +2,7 @@ pushd ..
 mkdir bin
 mkdir int
 popd
-g++ -DUNICODE -D_UNICODE -O2 -funroll-loops -finline-functions -c Window.cpp -o ../int/renderer.o -I../Libraries/GLAD/include/
+g++ -DUNICODE -D_UNICODE -O3 -march=native -flto -funroll-loops -fomit-frame-pointer -c Window.cpp -o ../int/renderer.o -I../Libraries/GLAD/include/
 iconv -f UTF-16LE -t UTF-8 ../Renderer.rc > ../resource.rc
 windres ../resource.rc -o ../int/resource.o
 g++ ../int/renderer.o ../int/resource.o -o ../bin/Renderer.exe -Wl,--subsystem,windows -luser32 -lkernel32 -lgdi32 -lopengl32
