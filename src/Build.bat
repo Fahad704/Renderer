3ac7810a8c99ff902b0b31d360f899084145a4ef
Fahad704 <shaikh.fahad.77377@gmail.com>
2024-11-09 00:20:17 +0530

    Updated batch files to run without GLAD

--- src/Build.bat
+++ src/Build.bat
@@ -2,9 +2,8 @@ pushd ..
 mkdir bin
 mkdir int
 popd
-g++ -DUNICODE -D_UNICODE -O3 -march=native -funroll-loops -c Window.cpp -o ../int/renderer.o -I../Libraries/GLAD/include/
+g++ -DUNICODE -D_UNICODE -O3 -march=native -funroll-loops -c Window.cpp -o ../int/renderer.o
 iconv -f UTF-16LE -t UTF-8 ../Renderer.rc > ../resource.rc
 windres ../resource.rc -o ../int/resource.o
-g++ -flto ../int/renderer.o ../int/resource.o -o ../bin/Renderer.exe -Wl,--subsystem,windows -luser32 -lkernel32 -lgdi32 -lopengl32
+g++ -flto ../int/renderer.o ../int/resource.o -o ../bin/Renderer.exe -Wl,--subsystem,windows -luser32 -lkernel32 -lgdi32
 "../bin/Renderer.exe"
-pause
--- src/build-no-icon.bat
+++ src/build-no-icon.bat
@@ -2,7 +2,5 @@ pushd ..
 mkdir bin
 mkdir int
 popd
-g++ -DUNICODE -D_UNICODE -O2 -funroll-loops -finline-functions -c Window.cpp -o ../int/renderer.o -I../Libraries/GLAD/include/
-g++ ../int/renderer.o -o ../bin/Renderer.exe -Wl,--subsystem,windows -luser32 -lkernel32 -lgdi32 -lopengl32
+g++ -DUNICODE -D_UNICODE -O2 -funroll-loops -finline-functions Window.cpp -o ../bin/Renderer.exe -Wl,--subsystem,windows -luser32 -lkernel32 -lgdi32 
 "../bin/Renderer.exe"
-pause
